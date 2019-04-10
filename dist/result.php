c39e54e03739946a6609eeae8379b1a193ca2bff
木村駿生 <>
2019-04-11 00:37:52 +0900

    bugfix

--- dist/result.php
+++ dist/result.php
@@ -11,7 +11,7 @@ $image_name = htmlspecialchars($_GET['image'], ENT_QUOTES, 'UTF-8');
     <!-- for twitter -->
     <meta name="twitter:card" content="summary_large_image" />
     <meta name="twitter:site" content="@MaedaDesignRoom" />
-    <meta property="og:url" content="http://test.com" />
+    <meta property="og:url" content=<?="http://".$_SERVER['HTTP_HOST'].$_SERVER["REQUEST_URI"]?> />
     <meta property="og:title" content="真！名刺交換" />
     <meta property="og:description" content="記事の要約（ディスクリプション）" />
     <meta property="og:image" content=<?="http://".$_SERVER['HTTP_HOST'].'/img/poses/'.$image_name?> />
