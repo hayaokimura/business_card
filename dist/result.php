edae54745926398d3e5526dbc27802a1b97a7177
木村駿生 <>
2019-05-12 22:28:40 +0900

    add images

--- dist/result.php
+++ dist/result.php
@@ -15,9 +15,36 @@ $image_name = htmlspecialchars($_GET['image'], ENT_QUOTES, 'UTF-8');
     <meta property="og:title" content="真！名刺交換" />
     <meta property="og:description" content="記事の要約（ディスクリプション）" />
     <meta property="og:image" content=<?="http://".$_SERVER['HTTP_HOST'].'/img/poses/'.$image_name?> />
+    <link rel="stylesheet" href="../css/reset.css">
+    <link rel="stylesheet" href="../css/main.css">
     <title>結果</title>
 </head>
 <body>
-  <img src=<?='../img/poses/'.$image_name?> />  
+  <header>
+      <div id='logo'>
+          <img src="../img/title.png" />
+      </div>
+  </header>
+  <div id='contents'>
+    <div class="title">
+        <p>こうやって、<span class="green underline">名刺</span>を渡そう！</p>
+        <p><span class="blue underline">今のキミらしさ</span>、伝えるために。</p>
+    </div>
+    <div class='result'><img src=<?='../img/poses/'.$image_name?> /></div>
+    <div class='tweet-button'>
+      <a href="https://twitter.com/share?ref_src=twsrc%5Etfw" class="twitter-share-button" 
+      data-url=<?="http://".$_SERVER['HTTP_HOST']?>
+      data-via='MaedaDesignRoom'
+      data-text='名刺の渡し方'
+      data-lang='ja'
+      data-show-count="false">Tweet
+      </a>
+    </div><script async src="https://platform.twitter.com/widgets.js" charset="utf-8"></script>
+    <a href='/'>
+      <div class="button" >
+          <span>もう一回！</span>
+      </div>
+    </a>
+  </div>
 </body>
 </html>
--- src/index.js
+++ src/index.js
@@ -7,14 +7,54 @@ class App extends React.Component {
         super(props);
         this.state = {
             filenames: [
-                "image1.jpg",
-                "image2.jpg",
-                "image3.jpg",
-                "image4.jpg",
-                "image5.jpg",
-                "image6.jpg",
-                "image7.jpg",
-                "image8.jpg",],
+                "13211dbca486.jpg",
+                "19b1ebc3444c.jpg",
+                "1b626a021380.jpg",
+                "265a28e7cb1b.jpg",
+                "2d4859993507.jpg",
+                "3976f3ee0060.jpg",
+                "40d1559728df.jpg",
+                "425de0e1c818.jpg",
+                "457cae78c6ba.jpg",
+                "4be15408e637.jpg",
+                "51c9556aeb6a.jpg",
+                "5640e9e3efca.jpg",
+                "608f6f7e661d.jpg",
+                "61c622f5fd6d.jpg",
+                "657c5de7f2b6.jpg",
+                "65df9e7234f9.jpg",
+                "690192d240c7.jpg",
+                "789edd5f148a.jpg",
+                "797e55d836ec.jpg",
+                "7ff69a879355.jpg",
+                "8480e2a86900.jpg",
+                "97332e14f237.jpg",
+                "9c210b9bb2e1.jpg",
+                "9dd1ee7ca95d.jpg",
+                "a761364ac43c.jpg",
+                "a80f1872f2d6.jpg",
+                "abb73c0abd30.jpg",
+                "ad6de7301226.jpg",
+                "b1a9af21729a.jpg",
+                "c294d9ff8f16.jpg",
+                "c33b6dc3ee4a.jpg",
+                "c77108b93fdb.jpg",
+                "c7a700323564.jpg",
+                "ca2a64b81414.jpg",
+                "cd0b80fc48cb.jpg",
+                "d36611f51b7a.jpg",
+                "d47a3bfb3703.jpg",
+                "d5857e02aa40.jpg",
+                "d61d4221cb6a.jpg",
+                "db2c243b20bd.jpg",
+                "df61371d6d22.jpg",
+                "ead9e99c043e.jpg",
+                "ee699f22613c.jpg",
+                "f0ee56c57027.jpg",
+                "f2ce32bc3377.jpg",
+                "f34400cd1c0b.jpg",
+                "f7b4ed55f828.jpg",
+                "fb8e35d97f6f.jpg",],
             fileindex: 0,
             changeImageHandler: null,
         }
@@ -48,7 +88,7 @@ class App extends React.Component {
         clearInterval(this.state.changeImageHandler);
         setTimeout(() => {
             window.location.href = 'dist/result.php\?image='+this.state.filenames[this.state.fileindex];
-          }, 1000);
+          }, 2000);
     }
     
     render() {
@@ -57,11 +97,12 @@ class App extends React.Component {
         return(
             <div>
                 <div className="title">
-                    <p>名刺交換ルーレット！</p>
+                    <p>こうやって、<span className="green underline">名刺</span>を渡そう！</p>
+                    <p><span className="blue underline">今のキミらしさ</span>、伝えるために。</p>
                 </div>
-                <Roulette filename={fileNames[fileIndex]} rouletteClick={this.clickRoulette} />
-                <div className="description">
-                    <p>タッチしてストップ！</p>
+                <Roulette filename={fileNames[fileIndex]} />
+                <div className="button" onClick={this.clickRoulette}>
+                    <span>ストップ！</span>
                 </div>
             </div>
         );
